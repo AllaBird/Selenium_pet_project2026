69bdc886ff84e014987b88491c7dc5ab40ff7341
AllaBird <allavorobieva@yahoo.com>
2026-02-23 23:13:03 -0500

    Add allure step annotations

--- src/test/java/com/ecommerce/page/CheckoutPage.java
+++ src/test/java/com/ecommerce/page/CheckoutPage.java
@@ -1,6 +1,7 @@
 package com.ecommerce.page;
 
 import com.ecommerce.page.base.BasePage;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,18 +12,21 @@ public class CheckoutPage extends BasePage {
         super(driver);
     }
 
+    @Step("Enter first name: {firstName}")
     public CheckoutPage fillFirstNameField(String firstName) {
         getWait2().until(ExpectedConditions.presenceOfElementLocated(By.id("billing_first_name"))).sendKeys(firstName);
 
         return this;
     }
 
+    @Step("Enter last name: {lastName}")
     public CheckoutPage fillLastNameField(String lastName) {
         getDriver().findElement(By.id("billing_last_name")).sendKeys(lastName);
 
         return this;
     }
 
+    @Step("Select country: {country}")
     public CheckoutPage fillCountryField(String country) {
         getDriver().findElement(By.id("billing_country")).click();
         getDriver().findElement(By.cssSelector("select2-search__field")).sendKeys(country );
@@ -30,16 +34,21 @@ public class CheckoutPage extends BasePage {
         return this;
     }
 
+    @Step("Enter street address: {address}")
     public CheckoutPage fillStreetAddressField(String address) {
         getDriver().findElement(By.id("billing_address_1")).sendKeys(address);
 
         return this;
+    }
 
-    }public CheckoutPage fillTownField(String town) {
+    @Step("Enter town/city: {town}")
+    public CheckoutPage fillTownField(String town) {
         getDriver().findElement(By.id("billing_city")).sendKeys(town);
 
         return this;
     }
+
+    @Step("Select state: {state}")
     public CheckoutPage fillStateField(String state) {
         getDriver().findElement(By.id("select2-billing_state-container")).sendKeys(state);
         getDriver().findElement(By.id("select2-billing_state-container")).sendKeys(state);
@@ -47,22 +56,24 @@ public class CheckoutPage extends BasePage {
         return this;
     }
 
+    @Step("Enter ZIP code: {zip}")
     public CheckoutPage fillZipField(String zip) {
         getDriver().findElement(By.id("billing_postcode")).sendKeys(zip);
 
         return this;
     }
 
+    @Step("Enter email address: {email}")
     public CheckoutPage fillEmailAddressField(String email) {
         getDriver().findElement(By.id("billing_email")).sendKeys(email);
 
         return this;
     }
 
+    @Step("Place the order")
     public CheckoutConfirmationPage clickPlaceOrder() {
         getWait5().until(ExpectedConditions.elementToBeClickable(By.id("place_order"))).click();
 
         return new CheckoutConfirmationPage(getDriver());
     }
-
 }
